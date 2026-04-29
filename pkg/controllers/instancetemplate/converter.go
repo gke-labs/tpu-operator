397125efe49465d666706e309cea95e33078e21f
Yuan Gao <ypgao@google.com>
2026-04-29 23:04:30 +0000

    feat: switch to computepb for InstanceTemplate converter
    Migrate converter.go from google.golang.org/api/compute/v1 to the
    newer cloud.google.com/go/compute/apiv1/computepb.
    
    This modernizes the codebase to use the Cloud Client proto structs.
    All supported fields are mapped, and tests have been updated.
    
    BUG=500811406
    Change-Id: I9431ebcf23c8f1f01e9a05ef70f46b584592ffe5
    
    Change-Id: I9676485df6bae9b195034c55b5e391ce3303b351

--- pkg/controllers/instancetemplate/converter.go
+++ pkg/controllers/instancetemplate/converter.go
@@ -4,7 +4,8 @@ import (
 	"sort"
 
 	tpuv1alpha1 "gke-internal.googlesource.com/tpu-node-group/pkg/apis/tpu/v1alpha1"
-	"google.golang.org/api/compute/v1"
+	computepb "cloud.google.com/go/compute/apiv1/computepb"
+	"k8s.io/utils/ptr"
 )
 
 const (
@@ -12,76 +13,72 @@ const (
 	reservationNameKey                     = "compute.googleapis.com/reservation-name"
 )
 
-
 // ToGCEInstanceTemplate converts an InstanceTemplate CR to GCE API InstanceTemplate.
-func ToGCEInstanceTemplate(cr *tpuv1alpha1.InstanceTemplate) *compute.InstanceTemplate {
-	properties := &compute.InstanceProperties{
-		MachineType: cr.Spec.MachineType,
+func ToGCEInstanceTemplate(cr *tpuv1alpha1.InstanceTemplate) *computepb.InstanceTemplate {
+	properties := &computepb.InstanceProperties{
+		MachineType: &cr.Spec.MachineType,
+		Metadata:    buildMetadata(cr.Spec.Metadata),
+		Disks:       buildBootDisk(cr.Spec.Image, cr.Spec.BootDiskSizeGB, cr.Spec.DiskType),
 	}
-
-	// Disks
-	properties.Disks = buildBootDisk(cr.Spec.Image, cr.Spec.BootDiskSizeGB, cr.Spec.DiskType)
 
 	// Scheduling
 	if cr.Spec.MaintenancePolicy != nil || cr.Spec.ProvisioningModel != nil {
-		scheduling := &compute.Scheduling{}
+		scheduling := &computepb.Scheduling{}
 		if cr.Spec.MaintenancePolicy != nil {
-			scheduling.OnHostMaintenance = *cr.Spec.MaintenancePolicy
+			scheduling.OnHostMaintenance = cr.Spec.MaintenancePolicy
 		}
 		if cr.Spec.ProvisioningModel != nil {
-			scheduling.ProvisioningModel = *cr.Spec.ProvisioningModel
+			scheduling.ProvisioningModel = cr.Spec.ProvisioningModel
 		}
 		properties.Scheduling = scheduling
 	}
 
+	// Reservation Affinity
+	if cr.Spec.Reservation != nil {
+		properties.ReservationAffinity = &computepb.ReservationAffinity{
+			ConsumeReservationType: ptr.To(consumeReservationTypeSpecificReservation),
+			Key:                    ptr.To(reservationNameKey),
+			Values:                 []string{*cr.Spec.Reservation},
+		}
+	}
+
 	// Network Interfaces
 	if cr.Spec.Subnetwork != nil {
-		properties.NetworkInterfaces = []*compute.NetworkInterface{
+		properties.NetworkInterfaces = []*computepb.NetworkInterface{
 			{
-				Subnetwork: *cr.Spec.Subnetwork,
+				Subnetwork: cr.Spec.Subnetwork,
 			},
 		}
 	}
 
-	// Metadata
-	properties.Metadata = buildMetadata(cr.Spec.Metadata)
-
 	// Tags
 	if len(cr.Spec.NetworkTags) > 0 {
-		properties.Tags = &compute.Tags{
+		properties.Tags = &computepb.Tags{
 			Items: cr.Spec.NetworkTags,
 		}
 	}
 
 	// Service Accounts
 	if len(cr.Spec.ServiceAccounts) > 0 {
-		var svcAccounts []*compute.ServiceAccount
+		var svcAccounts []*computepb.ServiceAccount
 		for _, sa := range cr.Spec.ServiceAccounts {
-			svcAccounts = append(svcAccounts, &compute.ServiceAccount{
-				Email:  sa.Email,
+			saEmail := sa.Email
+			svcAccounts = append(svcAccounts, &computepb.ServiceAccount{
+				Email:  &saEmail,
 				Scopes: sa.Scopes,
 			})
 		}
 		properties.ServiceAccounts = svcAccounts
 	}
 
-	// Reservation Affinity
-	if cr.Spec.Reservation != nil {
-		properties.ReservationAffinity = &compute.ReservationAffinity{
-			ConsumeReservationType: consumeReservationTypeSpecificReservation,
-			Key:                    reservationNameKey,
-			Values:                 []string{*cr.Spec.Reservation},
-		}
-	}
-
-	return &compute.InstanceTemplate{
-		Name:       cr.Name,
+	return &computepb.InstanceTemplate{
+		Name:       &cr.Name,
 		Properties: properties,
 	}
 }
 
 // buildMetadata constructs the metadata for GCE API, sorting keys for determinism.
-func buildMetadata(m map[string]string) *compute.Metadata {
+func buildMetadata(m map[string]string) *computepb.Metadata {
 	if len(m) == 0 {
 		return nil
 	}
@@ -91,35 +88,38 @@ func buildMetadata(m map[string]string) *compute.Metadata {
 	}
 	sort.Strings(keys)
 
-	items := make([]*compute.MetadataItems, 0, len(m))
+	items := make([]*computepb.Items, 0, len(m))
 	for _, k := range keys {
 		v := m[k]
-		items = append(items, &compute.MetadataItems{
-			Key:   k,
-			Value: &v,
+		key := k
+		val := v
+		items = append(items, &computepb.Items{
+			Key:   &key,
+			Value: &val,
 		})
 	}
-	return &compute.Metadata{Items: items}
+	return &computepb.Metadata{Items: items}
 }
 
 // buildBootDisk constructs the disks slice for GCE API.
-func buildBootDisk(image *string, bootDiskSizeGB *int32, diskType *string) []*compute.AttachedDisk {
+func buildBootDisk(image *string, bootDiskSizeGB *int32, diskType *string) []*computepb.AttachedDisk {
 	if image == nil && bootDiskSizeGB == nil && diskType == nil {
 		return nil
 	}
 
-	disk := &compute.AttachedDisk{
-		Boot: true,
-		InitializeParams: &compute.AttachedDiskInitializeParams{},
+	disk := &computepb.AttachedDisk{
+		Boot: ptr.To(true),
+		InitializeParams: &computepb.AttachedDiskInitializeParams{},
 	}
 	if image != nil {
-		disk.InitializeParams.SourceImage = *image
+		disk.InitializeParams.SourceImage = image
 	}
 	if bootDiskSizeGB != nil {
-		disk.InitializeParams.DiskSizeGb = int64(*bootDiskSizeGB)
+		size := int64(*bootDiskSizeGB)
+		disk.InitializeParams.DiskSizeGb = &size
 	}
 	if diskType != nil {
-		disk.InitializeParams.DiskType = *diskType
+		disk.InitializeParams.DiskType = diskType
 	}
-	return []*compute.AttachedDisk{disk}
+	return []*computepb.AttachedDisk{disk}
 }
