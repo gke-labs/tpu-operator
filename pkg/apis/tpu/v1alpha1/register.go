2d0845563739aab2f3d280beb20daf32d7391c84
Yuan Gao <ypgao@google.com>
2026-05-13 05:39:16 +0000

    feat(apis): introduce ManagedInstanceGroup CRD and converters
    Introduce the ManagedInstanceGroup Custom Resource Definition along with
    conversion logic to map Kubernetes CRs to computepb structures.
    
    Bug: b/505013584
    CONV=e23107ca-dc51-4180-8e8a-e2f76ca23efd
    
    Change-Id: I686fa0d1aaa872d6f36dfa08ee88817e5db0b97f

--- pkg/apis/tpu/v1alpha1/register.go
+++ pkg/apis/tpu/v1alpha1/register.go
@@ -35,6 +35,8 @@ func addKnownTypes(scheme *runtime.Scheme) error {
 		&InstanceTemplateList{},
 		&WorkloadPolicy{},
 		&WorkloadPolicyList{},
+		&ManagedInstanceGroup{},
+		&ManagedInstanceGroupList{},
 	)
 	metav1.AddToGroupVersion(scheme, SchemeGroupVersion)
 	return nil
