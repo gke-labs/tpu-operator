4d2e8ccd3ef835390e27c758dfc697221ff814c4
Yuan Gao <ypgao@google.com>
2026-05-14 16:28:39 +0000

    fix: fix GCE test build error inherited from base
    Change-Id: I9295a5e8457d09dd6011b6300acfdcc145e9853e

--- pkg/gce/client_test.go
+++ pkg/gce/client_test.go
@@ -329,12 +329,22 @@ func TestNewManager(t *testing.T) {
 			return nil, fmt.Errorf("should not be called")
 		}
 
+		initResourcePoliciesDummy := func(ctx context.Context, opts ...option.ClientOption) (*compute.ResourcePoliciesClient, error) {
+			return nil, fmt.Errorf("should not be called")
+		}
+
+		initRegionOperationsDummy := func(ctx context.Context, opts ...option.ClientOption) (*compute.RegionOperationsClient, error) {
+			return nil, fmt.Errorf("should not be called")
+		}
+
 		_, err := newManagerWithConstructors(
 			ctx,
 			initIGMSuccess,
 			initInstancesFailure,
 			initInstanceTemplatesDummy,
 			initGlobalOperationsDummy,
+			initResourcePoliciesDummy,
+			initRegionOperationsDummy,
 			onClientCloseForTest,
 		)
 
